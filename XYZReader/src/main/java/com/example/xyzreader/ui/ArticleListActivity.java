2edfc3853c72f26ee07f8812f68c5171caa43aad
farmaker47 <farmaker47@yahoo.cm>
2018-04-17 17:55:36 +0300

    First screen changed to Coord

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -55,7 +55,7 @@ public class ArticleListActivity extends ActionBarActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_article_list);
+        setContentView(R.layout.activity_article_list_soloupis);
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
